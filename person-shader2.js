04bebbc00d8472a46e7569234f0f801b281bb26a
mashaojie <mashaojie@pinefield-inc.com>
2021-08-24 20:51:35 +0800

    纹理方块贴图实现人员数据定位

--- person-shader2.js
+++ person-shader2.js
@@ -5,23 +5,23 @@ export const PersonShader = {
     }
   },
   vertexShader:
-    /* glsl */
+  /* glsl */
     `
-    attribute vec3 currentPosition;
     attribute float size;
-    attribute vec4 complementaryColor;
+    attribute vec4 color;
+    attribute vec3 currentPosition;
 
     varying vec4 vColor;
     varying vec2 vUv;
 
     void main() {
-      vColor = complementaryColor;
+      vColor = color;
       vUv = uv;
-      vec3 vPosition = position + currentPosition;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      vec3 vPosition = currentPosition + position * size;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
     }`,
   fragmentShader:
-    /* glsl */
+  /* glsl */
     `
     uniform sampler2D pointTexture;
 
